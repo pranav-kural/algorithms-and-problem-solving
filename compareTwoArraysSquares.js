8d2d35544dfa70c5e99d03667fba366e5f8b990f
Pranav Kural <pranavkural042@gmail.com>
2021-10-03 08:50:22 +0000

    problem starter comparing two arrays squares

--- compareTwoArraysSquares.js
+++ compareTwoArraysSquares.js
@@ -1,14 +1,20 @@
 /* Given two arrays, find out if for each element in one array, 
 there is a corresponding sqaure value in the other array */
+
+// Problem understanding and planning
+
+// example INPUTs
 // let arr1 = [1,2,1]
 // let arr2 = [4,1,1]
 
 // each number of first array, has a corresponding sqaure value in the second array
 
+// example OUTPUTs
 // [1,2,3] -> [1,4,9] = true
 // [1,2,1] -> [4,1,4] = false
 // [1,3]   -> [1,9]   = true
 
+// PSUEDO
 // if arrays are empty or nullish
 // OR if size of the arrays is not same
 // OR if any of the arrays contain anything other than a number
