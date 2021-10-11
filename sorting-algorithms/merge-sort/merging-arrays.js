460ff54d10dcc1bdd74ef671b5e9acad8985d46a
pranav-kural <pranavkural042@gmail.com>
2021-10-12 00:18:13 +0530

    handling extreme cases of empty or null array

--- sorting-algorithms/merge-sort/merging-arrays.js
+++ sorting-algorithms/merge-sort/merging-arrays.js
@@ -7,31 +7,33 @@
  */
 
 const mergeSortedArrays = (arr1, arr2) => {
+
+    // if any of the arrays are null or input provided not array
+    if (!(Array.isArray(arr1)) || !(Array.isArray(arr2))) return [];
+
     // initialize variables
     let i = 0;  // pointer for arr1
     let j = 0;  // pointer for arr2
-    const sortedArray = []; // for holding sorted values
+    const mergedArray = []; // for holding sorted values
     // loop through elements of arr1 & arr2
     while (i < arr1.length && j < arr2.length) {
         // if element of arr1 is less than element of arr2
         if (arr1[i] <= arr2[j]) {
-            sortedArray.push(arr1[i]);
+            mergedArray.push(arr1[i]);
             i++;    // move to next element of arr1 in next iteration
          } else {
-             sortedArray.push(arr2[j])
+             mergedArray.push(arr2[j])
              j++;   // move to next element of arr2 in next iteration
          }
-         if (i == arr1.length) {
-             sortedArray.push(...arr2.slice(j, arr2.length));
-             break;
-         } else if (j == arr2.length) {
-            sortedArray.push(...arr1.slice(i, arr1.length));
-         }
     }
-    return sortedArray;
+    // to handle cases of empty arrays, or where above while exists before merging all items
+    (i < arr1.length) ? mergedArray.push(...arr1.slice(i)) : mergedArray.push(...arr2.slice(j));
+    
+    // return the merged array
+    return mergedArray;
 };
 
 // test
-// console.log("Merged array: ", mergeSortedArrays([1,3,4,80],[3,15,99,102]));
+console.log("Merged array: ", mergeSortedArrays([1,3,4,80],[3,15,99,102]));
 
 module.exports = { mergeSortedArrays };
